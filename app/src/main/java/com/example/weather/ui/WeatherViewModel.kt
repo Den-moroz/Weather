7df11f958635d838c98711223c96613e8aa5955b
Denis Moroz <moroz06denis@gmail.com>
2023-10-20 15:28:39 +0200

    added check for handling bad internet connection

--- app/src/main/java/com/example/weather/ui/WeatherViewModel.kt
+++ app/src/main/java/com/example/weather/ui/WeatherViewModel.kt
@@ -11,12 +11,18 @@ import kotlinx.coroutines.launch
 
 const val TAG = "WeatherViewModel";
 
+enum class WeatherApiStatus { LOADING, ERROR, DONE }
+
 class WeatherViewModel: ViewModel() {
     private val _dailyWeatherData = MutableLiveData<WeatherResponse>()
     val dailyWeatherData: LiveData<WeatherResponse> get() = _dailyWeatherData
 
     private val _weeklyWeatherData = MutableLiveData<WeatherResponse>()
     val weeklyWeatherData: LiveData<WeatherResponse> get() = _weeklyWeatherData
+
+    private val _status = MutableLiveData<WeatherApiStatus>()
+
+    val status: LiveData<WeatherApiStatus> = _status
 
     private val _location = MutableLiveData("London")
     val location: LiveData<String> get() = _location
@@ -29,10 +35,13 @@ class WeatherViewModel: ViewModel() {
 
     fun getDailyWeather() {
         viewModelScope.launch {
+            _status.value = WeatherApiStatus.LOADING
             try {
                 val locationValue = location.value ?: "London"
                 _dailyWeatherData.value = weatherApi.getDailyWeather(locationValue)
+                _status.value = WeatherApiStatus.DONE
             } catch (e: Exception) {
+                _status.value = WeatherApiStatus.ERROR
                 Log.e(TAG, "Something went wrong when trying to execute network request for daily weather")
             }
         }
@@ -40,10 +49,13 @@ class WeatherViewModel: ViewModel() {
 
     fun getWeeklyWeatherData() {
         viewModelScope.launch {
+            _status.value = WeatherApiStatus.LOADING
             try {
                 val locationValue = location.value ?: "London"
                 _weeklyWeatherData.value = weatherApi.getWeeklyWeather(locationValue)
+                _status.value = WeatherApiStatus.DONE
             } catch (e: Exception) {
+                _status.value = WeatherApiStatus.ERROR
                 Log.e(TAG, "Something went wrong when trying to execute network request for weekly weather")
             }
         }
